4c4323a46359468436395d98606b2bc6ae8b0447
Francisco José Fajardo Toril <frannnlol@gmail.com>
2018-02-27 12:37:37 +0100

    Changes on menu button logic; Using angular directives.

--- src/app/menu-button/menu-button.component.ts
+++ src/app/menu-button/menu-button.component.ts
@@ -6,28 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-button.component.scss']
 })
 export class MenuButtonComponent implements OnInit {
-  menu_is_active: boolean;
+  active: boolean;
 
   //Constructor
   constructor() { }
 
   // Component setup
   ngOnInit() {
-    this.menu_is_active = false;
-  }
-
-  // Function called when user clicks the button. It toggles menu state
-  // and opens or closes it.
-  toggleState() {
-    this.menu_is_active = this.menu_is_active === false ? true : false;
-    this.menu_is_active === false ? this.openMenu() : this.closeMenu();
-  }
-
-  openMenu(){
-    console.log("Open Menu");
-  }
-
-  closeMenu(){
-    console.log("Close Menu");
+    this.active = false;
   }
 }
